934e2ba0518bc29ba1102847b356294bd0d7130c
Atul Gairola <atulgairola08@gmail.com>
2020-06-04 21:57:18 +0530

    Updates

---
+++ precache-manifest.3d09597c75b3f757fefe29f33313c27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32fff85ba8a75e66b341f8cdb557280",
+    "url": "/covid-19-india-dashboard/index.html"
+  },
+  {
+    "revision": "6d0de854312157d82a98",
+    "url": "/covid-19-india-dashboard/static/css/main.cf64e92c.chunk.css"
+  },
+  {
+    "revision": "00ef3f4a7e95c115d2fa",
+    "url": "/covid-19-india-dashboard/static/js/2.1451f43e.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19-india-dashboard/static/js/2.1451f43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0de854312157d82a98",
+    "url": "/covid-19-india-dashboard/static/js/main.40e35563.chunk.js"
+  },
+  {
+    "revision": "165aed4cef423eab2b52",
+    "url": "/covid-19-india-dashboard/static/js/runtime-main.aa67c05b.js"
+  }
+]);
